ddf8470ef1aa519cf146b79345081ff7890b6aeb
Mark Fang <microspaze@qq.com>
2024-05-04 22:09:18 +0800

    Fix crop transformation demo not work.

--- samples/Sample/Pages/Transformations/CropTransformationPage.xaml.cs
+++ samples/Sample/Pages/Transformations/CropTransformationPage.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using FFImageLoading;
+using FFImageLoading.Helpers;
+using FFImageLoading.Transformations;
 
 namespace Sample
 {
 	public partial class CropTransformationPage : ContentPage
 	{
+		private readonly IImageService _imageService = ServiceHelper.GetService<IImageService>();
+
 		CropTransformationPageModel viewModel;
 
 		public CropTransformationPage()
@@ -35,6 +40,26 @@ namespace Sample
 			base.OnAppearing();
 
 			viewModel.ReloadImage();
+		}
+
+		private void OnTestPngClicked(object sender, EventArgs e)
+		{
+			viewModel.ImageUrl = "no_avatar.png";
+			viewModel.ReloadImage();
+		}
+
+		private async void TestCropSize(object sender, EventArgs e)
+		{
+			if (_imageService == null) return;
+
+			var imagePath = "maui_beach.jpg";
+			var original = await _imageService.LoadFile(imagePath).AsJPGStreamAsync(_imageService, 100);
+			var jpgStream = await _imageService
+				.LoadFile(imagePath)
+				.Transform(new CropTransformation(1, 0, 0))
+				.AsJPGStreamAsync(_imageService, 100);
+			viewModel.OriginalSize = original.Length;
+			viewModel.CroppedSize = jpgStream.Length;
 		}
 	}
 }
--- samples/Sample/Pages/Transformations/CropTransformationPageModel.cs
+++ samples/Sample/Pages/Transformations/CropTransformationPageModel.cs
@@ -7,33 +7,40 @@ using FFImageLoading.Work;
 
 namespace Sample
 {
-
     public partial class CropTransformationPageModel : ObservableObject
     {
-        double mX = 0f;
-        double mY = 0f;
-        double mRatioPan = -0.0015f;
+        double mDeltaPan = 0.02f;
         double mRatioZoom = 0.8f;
 
-		public ObservableCollection<ITransformation> Transformations { get; set; } = new ();
+        [ObservableProperty]
+        long originalSize;
+
+        [ObservableProperty]
+        long croppedSize;
 
 		[ObservableProperty]
         string imageUrl;
 
+        [ObservableProperty]
+        List<ITransformation> transformations;
 
 		public Action ReloadImageHandler { get; set; }
 
-        public void ReloadImage()
-        {
+		public CropTransformationPageModel()
+		{
 			ImageUrl = "https://loremflickr.com/600/600/nature?filename=crop_transformation.jpg";
 
 			CurrentZoomFactor = 1d;
 			CurrentXOffset = 0d;
 			CurrentYOffset = 0d;
+		}
 
-			Transformations.Clear();
-            Transformations.Add(
-                new CropTransformation(CurrentZoomFactor, CurrentXOffset, CurrentYOffset, 1f, 1f));
+        public void ReloadImage()
+        {
+			Transformations = new List<ITransformation>()
+			{
+				new CropTransformation(CurrentZoomFactor, CurrentXOffset, CurrentYOffset, 1f, 1f)
+			};
 
 			ReloadImageHandler?.Invoke();
         }
@@ -49,33 +56,35 @@ namespace Sample
 
         public void OnPanUpdated(PanUpdatedEventArgs e)
         {
-            if (e.StatusType == GestureStatus.Completed)
+	        Console.WriteLine($"Pan status: {e.StatusType}");
+            if (e.StatusType == GestureStatus.Running)
             {
-                mX = CurrentXOffset;
-                mY = CurrentYOffset;
-            }
-            else if (e.StatusType == GestureStatus.Running)
-            {
-                CurrentXOffset = (e.TotalX * mRatioPan) + mX;
-                CurrentYOffset = (e.TotalY * mRatioPan) + mY;
+                var xChange = Math.Abs(e.TotalX);
+                var yChange = Math.Abs(e.TotalY);
+                if (xChange >= yChange)
+                {
+	                CurrentXOffset += e.TotalX > 0 ? -mDeltaPan : mDeltaPan;
+                }
+                if (xChange <= yChange)
+                {
+	                CurrentYOffset += e.TotalY > 0 ? -mDeltaPan : mDeltaPan;
+                }
                 ReloadImage();
             }
         }
 
         public void OnPinchUpdated(PinchGestureUpdatedEventArgs e)
         {
-            if (e.Status == GestureStatus.Completed)
-            {
-                mX = CurrentXOffset;
-                mY = CurrentYOffset;
-            }
-            else if (e.Status == GestureStatus.Running)
+	        Console.WriteLine($"Pinch status: {e.Status}");
+            if (e.Status == GestureStatus.Running)
             {
                 CurrentZoomFactor += (e.Scale - 1) * CurrentZoomFactor * mRatioZoom;
                 CurrentZoomFactor = Math.Max(1, CurrentZoomFactor);
-
-                CurrentXOffset = (e.ScaleOrigin.X * mRatioPan) + mX;
-                CurrentYOffset = (e.ScaleOrigin.Y * mRatioPan) + mY;
+                if (CurrentZoomFactor == 1)
+                {
+	                CurrentXOffset = 0;
+	                CurrentYOffset = 0;
+                }
                 ReloadImage();
             }
         }
